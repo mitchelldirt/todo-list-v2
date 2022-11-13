44b39a9efd78e2a839d0e5706b16e699f85a16bf
Mitchell Mudd <mitchellmudd@outlook.com>
2022-11-12 20:52:15 -0500

    Changed express server to use controllers. Going to add more tomorrow

--- app/src/app.js
+++ app/src/app.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/v1', indexRouter);
+app.use('/', indexRouter);
 
 export default app;
