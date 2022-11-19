1c1c12cc8a4e9c36ca2f958c1d15d99a4b1e06de
Mitchell Mudd <mitchellmudd@outlook.com>
2022-11-19 16:33:25 -0500

    Added an async middleware function

--- app/src/routes/message.js
+++ app/src/routes/message.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { addMessage, messagesPage } from '../controllers';
-import { appendSays } from '../middleware/middleware';
+import { appendSays, performAsyncAction } from '../middleware/middleware';
 
 const messageRouter = express.Router();
 
 messageRouter.get('/', messagesPage);
-messageRouter.post('/', appendSays, addMessage);
+messageRouter.post('/', appendSays, performAsyncAction, addMessage);
 
 export default messageRouter;
