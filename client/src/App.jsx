57911509d58629e75def8beced642b47f48f7d01
Mitchell Mudd <mitchellmudd@outlook.com>
2022-12-06 17:19:35 -0500

    Got Tanstack Query up and running in less than 30 minutes 🔥🚀

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,74 +3,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { TaskList } from './components/main/task/taskList'
-// console.log(await postResponse.text())
-// console.log(await getResponse.text())
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
-const sampleData = [
-  {
-    taskID: 1,
-    title: 'Work on your ToDo List Project',
-    description: 'Make some mock data to work with for the front end',
-    dueDate: '2022-12-13 19:00:00',
-    priority: 3,
-    userID: '124awfh3435u465ijerfh',
-    projectID: 1
-  },
-  {
-    taskID: 2,
-    title: 'Learn TypeScript',
-    description: 'Learn how to use typescript with JSX (aka TSX)',
-    dueDate: '2022-12-13 14:00:00',
-    priority: 3,
-    userID: '124awfh3435u465ijerfh',
-    projectID: 1
-  },
-  {
-    taskID: 3,
-    title: 'Finish your Shopify store',
-    description: 'Will provide value to the company',
-    dueDate: '2022-02-11 19:00:00',
-    priority: 4,
-    userID: '124awfh3435u465ijerfh',
-    projectID: 2
-  },
-  {
-    taskID: 4,
-    title: 'Create awesome automation in Jira',
-    description: 'Create an automation to rule all automation',
-    dueDate: '2022-12-22 19:00:00',
-    priority: 2,
-    userID: '124awfh3435u465ijerfh',
-    projectID: 2
-  },
-  {
-    taskID: 5,
-    title: 'Send the green V4',
-    description: 'Send the green V4 by being more confident in your footing',
-    dueDate: '2022-12-15 19:00:00',
-    priority: 1,
-    userID: '124awfh3435u465ijerfh',
-    projectID: 3
-  },
-  {
-    taskID: 6,
-    title: 'Send your first V5',
-    description: "Keep working on V4 climbs until you feel pretty good about them, then it's V5 time!",
-    dueDate: '2022-01-13 19:00:00',
-    priority: 5,
-    userID: '124awfh3435u465ijerfh',
-    projectID: 3
-  }
-]
+const queryClient = new QueryClient()
+
 
 function App() {
   return (
-    <div className="App">
-      <TaskList
-      taskData={sampleData}
-      />
-     </div> 
-    
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <TaskList
+
+        />
+      </div>
+    </QueryClientProvider>
   )
 }
 
--- client/src/components/main/task/taskList.jsx
+++ client/src/components/main/task/taskList.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import { Task } from './task';
+import { useQuery } from '@tanstack/react-query'
 
+const fetchTodoList = async() => {
+  const data = await fetch('http://localhost:3000/task', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'text/plain'
+      }
+});
+return data.json();
+}
 
-export const TaskList = ({ taskData }) => {
-  return (taskData.map(task => <Task
+export const TaskList = () => {
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ['todos'],
+    queryFn: fetchTodoList,
+  })
+
+  if (isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+
+console.log(data)
+  return (data.tasks.map(task => <Task
     key={task.id}
     title={task.title}
     description={task.description}
